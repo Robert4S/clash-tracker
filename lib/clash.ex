f7989e4c557f6bc0790917d5616d40d497ab7363
Robert4S <robertschmidt4th@gmail.com>
2025-02-17 16:33:49 +0100

    test

--- lib/clash.ex
+++ lib/clash.ex
@@ -3,8 +3,6 @@ defmodule Clash do
   Clash keeps the contexts that define your domain
   and business logic.
 
-  This is a change
-  This too
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
